e61a59dc049945a0798dd0c0677be1dd73fab21a
MATEUS COSTA DOS SANTOS <mcosta0131@gmail.com>
2019-08-27 10:22:21 -0300

    First commit

---
+++ tec-university/src/main/java/com/tecuniversity/tecuniversity/TecUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.tecuniversity.tecuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecUniversityApplication.class, args);
+	}
+
+}
---
+++ tec-university/src/test/java/com/tecuniversity/tecuniversity/TecUniversityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tecuniversity.tecuniversity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TecUniversityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
